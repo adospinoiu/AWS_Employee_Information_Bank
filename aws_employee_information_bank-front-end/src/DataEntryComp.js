a0d4fc933911bd7a6220322912d50fdd3ec1a3c0
adospinoiu <alex.dospinoiu@gmail.com>
2022-09-20 05:04:25 -0700

    Created all the useState variables for the DataEntry Comp

--- aws_employee_information_bank-front-end/src/DataEntryComp.js
+++ aws_employee_information_bank-front-end/src/DataEntryComp.js
@@ -2,10 +2,46 @@ import React, { useState } from 'react';
 // import axios from './axios';
 import './DataEntryComp.css';
 
-function DataEntry({}) {
+function DataEntry({ }) {
+    // Set State for the Input Fields
+    const [ empFirstName, setEmpFirstName ] = useState([])
+    const [ empLastName, setEmpLastName ] = useState([])
+    const [ empDateOfBirth, setEmpDateOfBirth ] = useState([])
+    const [ empStreetAddress, setEmpStreetAddress ] = useState([])
+    const [ empCity, setEmpCity ] = useState([])
+    const [ empState, setEmpState ] = useState([])
+    const [ empZipCode, setEmpZipCode ] = useState([])
+
     return (
-        <div className='main_div'>
-            <p>Data Entry</p>
+        <div className='dataEntry_main'>
+            <div className='dataEntry_header'>
+                <p>Enter New Employee Information</p>
+            </div>
+
+            <div className='dataEntry_inputs'>
+                <input
+                    value={title}
+                    placeholder="Title"
+                    type="text"
+                    onChange={e => setTitle(e.target.value)}
+                />
+
+                <input
+                    value={author}
+                    placeholder="Author"
+                    type="text"
+                    onChange={e => setAuthor(e.target.value)}
+                />
+
+                <input
+                    value={recommended}
+                    placeholder="Recommended by"
+                    type="text"
+                    onChange={e => setRecommended(e.target.value)}
+                />
+
+                <button onClick={sendNewBook} type="submit">Add to List</button>
+            </div>
         </div>
     )
 }
