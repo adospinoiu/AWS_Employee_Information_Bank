555db75b61835b8bbe4595922def77878b718ec5
adospinoiu <alex.dospinoiu@gmail.com>
2022-09-19 17:42:20 -0700

    Created the two main components and their css files

---
+++ aws_employee_information_bank-front-end/src/DataEntryComp.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+// import axios from './axios';
+import './DataEntryComp.css';
+
+function DataEntry({}) {
+    return (
+        <div>
+            <p>Data Entry</p>
+        </div>
+    )
+}
+
+export default DataEntry
---
+++ aws_employee_information_bank-front-end/src/DataViewEditComp.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+// import axios from './axios';
+import './DataViewEditComp.css';
+
+function DataViewEdit({}) {
+    return (
+        <div>
+            <p>Data View And Edit</p>
+        </div>
+    )
+}
+
+export default DataViewEdit
