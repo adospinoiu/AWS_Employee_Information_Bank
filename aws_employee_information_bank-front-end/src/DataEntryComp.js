957deb5e0004fce9fee7203b3a835bb4aa55df57
adospinoiu <alex.dospinoiu@gmail.com>
2022-09-20 05:44:26 -0700

    Created all the input fields and the submit button

--- aws_employee_information_bank-front-end/src/DataEntryComp.js
+++ aws_employee_information_bank-front-end/src/DataEntryComp.js
@@ -6,11 +6,29 @@ function DataEntry({ }) {
     // Set State for the Input Fields
     const [ empFirstName, setEmpFirstName ] = useState([])
     const [ empLastName, setEmpLastName ] = useState([])
-    const [ empDateOfBirth, setEmpDateOfBirth ] = useState([])
+    const [ empDateOfHire, setEmpDateOfHire ] = useState([])
     const [ empStreetAddress, setEmpStreetAddress ] = useState([])
     const [ empCity, setEmpCity ] = useState([])
     const [ empState, setEmpState ] = useState([])
     const [ empZipCode, setEmpZipCode ] = useState([])
+
+    // const addNewEmployee = (e) => {
+    //     e.preventDefault();
+
+    //     console.log(empFirstName);
+
+    //     setEmpFirstName('');
+    //     setEmpLastName('');
+    //     setEmpDateOfHire('');
+    //     setEmpStreetAddress('');
+    //     setEmpCity('');
+    //     setEmpState('');
+    //     setEmpZipCode('');
+    // }
+
+    const addNewEmployee = () => {
+        console.log(empFirstName)
+    }
 
     return (
         <div className='dataEntry_main'>
@@ -20,27 +38,57 @@ function DataEntry({ }) {
 
             <div className='dataEntry_inputs'>
                 <input
-                    value={title}
-                    placeholder="Title"
+                    value={empFirstName}
+                    placeholder="Employee First Name"
                     type="text"
-                    onChange={e => setTitle(e.target.value)}
+                    onChange={e => setEmpFirstName(e.target.value)}
                 />
 
                 <input
-                    value={author}
-                    placeholder="Author"
+                    value={empLastName}
+                    placeholder="Employee Last Name"
                     type="text"
-                    onChange={e => setAuthor(e.target.value)}
+                    onChange={e => setEmpLastName(e.target.value)}
                 />
 
                 <input
-                    value={recommended}
-                    placeholder="Recommended by"
+                    value={empDateOfHire}
+                    placeholder="Employee Date of Hire"
                     type="text"
-                    onChange={e => setRecommended(e.target.value)}
+                    onChange={e => setEmpDateOfHire(e.target.value)}
                 />
 
-                <button onClick={sendNewBook} type="submit">Add to List</button>
+                <input
+                    value={empStreetAddress}
+                    placeholder="Employee Street Address"
+                    type="text"
+                    onChange={e => setEmpStreetAddress(e.target.value)}
+                />
+
+                <input
+                    value={empCity}
+                    placeholder="Employee City"
+                    type="text"
+                    onChange={e => setEmpCity(e.target.value)}
+                />
+
+                <input
+                    value={empState}
+                    placeholder="Employee State"
+                    type="text"
+                    onChange={e => setEmpState(e.target.value)}
+                />
+
+                <input
+                    value={empZipCode}
+                    placeholder="Employee Zip Code"
+                    type="text"
+                    onChange={e => setEmpZipCode(e.target.value)}
+                />
+            </div>
+
+            <div className='dataEntry_submit'>
+                <button onClick={addNewEmployee} type="submit">Add New Employee</button>
             </div>
         </div>
     )
